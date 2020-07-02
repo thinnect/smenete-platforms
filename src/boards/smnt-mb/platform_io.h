05043c1a3f640de85f146df6cf8353b09b9daf85
Raido Pahtma <raidopahtma@gmail.com>
2020-07-02 08:57:46 +0300

    List ADCs in platform_io.h

--- src/boards/smnt-mb/platform_io.h
+++ src/boards/smnt-mb/platform_io.h
@@ -9,6 +9,7 @@
 #define PLATFORM_IO_H_
 
 #include "em_gpio.h"
+#include "em_adc.h"
 
 #define PLATFORM_LED_COUNT 2
 #define PLATFORM_LED0_PORT gpioPortF
@@ -27,5 +28,11 @@
 // No button
 //#define PLATFORM_BUTTON_PORT gpioPortF
 //#define PLATFORM_BUTTON_PIN  4
+
+#define PLATFORM_ADC_COUNT 2
+// 0: MCU ADC
+// 1: External I2C ADC on SM1
+// TODO external needs a better fake-pointer-marker than NULL probably
+#define PLAFTORM_ADCS { ADC0, NULL }
 
 #endif//PLATFORM_IO_H_
