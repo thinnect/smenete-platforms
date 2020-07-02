1526f3fa0cfea03eff273f34fc76647451f53a49
Raido Pahtma <raidopahtma@gmail.com>
2020-07-02 12:08:41 +0300

    Rework sensor power config. Add defines for pins.

--- src/boards/smnt-mb/mw_setup_sm3.c
+++ src/boards/smnt-mb/mw_setup_sm3.c
@@ -10,6 +10,8 @@
 #define __LOG_LEVEL__ (LOG_LEVEL_mw_setup & BASE_LOG_LEVEL)
 #include "log.h"
 
+#include "platform_io.h"
+
 #include "mw_vehicle_sensor.h"
 
 #include "cmsis_os2.h"
@@ -17,23 +19,27 @@
 /**
  * Microwave setup is a bit complicated on SM3, there is the regulator control,
  * and separately the sensor control, even though the sensor is the only thing
- * using the regulatr. On top of that, the regulator must be in PWM mode, which
- * is not very efficient, but burst mode can cause noise in the MW sensor.
+ * using the regulator. On top of that, the other regulator (3V3) must be in
+ * PWM mode, which is not very efficient, but burst mode can cause noise in the
+ * MW sensor.
  */
-static void semw_power_control(bool enable)
+void semw_power_control(bool enable)
 {
 	debug1("sensor pwr %d", (int)enable);
 
 	if(enable)
 	{
 		// Enable 5V regulator
-		GPIO_PinModeSet(gpioPortB, 13, gpioModePushPull, 1);
+		GPIO_PinModeSet(POWER_CONTROL_5V_ENABLE_PORT, POWER_CONTROL_5V_ENABLE_PIN,
+		                gpioModePushPull, 1);
 
 		// Set regulator to PWM mode
-		GPIO_PinModeSet(gpioPortF, 4, gpioModePushPull, 1);
+		GPIO_PinModeSet(POWER_CONTROL_3V3_PWM_MODE_ENABLE_PORT, POWER_CONTROL_3V3_PWM_MODE_ENABLE_PIN,
+		                gpioModePushPull, 1);
 
 		// Enable MW sensor
-		GPIO_PinModeSet(gpioPortD, 12, gpioModePushPull, 1);
+		GPIO_PinModeSet(SEMW_SENSOR_ENABLE_PORT, SEMW_SENSOR_ENABLE_PIN,
+		                gpioModePushPull, 1);
 
 		// Wait a bit
 		osDelay(60);
@@ -41,23 +47,15 @@ static void semw_power_control(bool enable)
 	else
 	{
 		// Disable MW sensor
-		GPIO_PinModeSet(gpioPortD, 12, gpioModePushPull, 0);
+		GPIO_PinModeSet(SEMW_SENSOR_ENABLE_PORT, SEMW_SENSOR_ENABLE_PIN,
+		                gpioModePushPull, 0);
 
 		// Set regulator to burst mode
-		GPIO_PinModeSet(gpioPortF, 4, gpioModePushPull, 0);
+		GPIO_PinModeSet(POWER_CONTROL_3V3_PWM_MODE_ENABLE_PORT, POWER_CONTROL_3V3_PWM_MODE_ENABLE_PIN,
+		                gpioModePushPull, 0);
 
 		// Disable 5V regulator as it is not used for anything else
-		GPIO_PinModeSet(gpioPortB, 13, gpioModePushPull, 0);
+		GPIO_PinModeSet(POWER_CONTROL_5V_ENABLE_PORT, POWER_CONTROL_5V_ENABLE_PIN,
+		                gpioModePushPull, 0);
 	}
-}
-
-void mw_sensor_setup()
-{
-	semw_input_config_t icfg;
-	icfg.gpio_port = gpioPortD;
-	icfg.gpio_pin = 14;
-	icfg.adc_scan_group = adcScanInputGroup0;
-	icfg.adc_pos_sel = adcPosSelAPORT3XCH6;
-
-	semw_init(&icfg, &semw_power_control);
 }
