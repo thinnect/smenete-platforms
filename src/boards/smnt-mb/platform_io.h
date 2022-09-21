dc98a520dccae97d2ed74a0e495fb504ffe3a4c9
German <german.mundinger@gmail.com>
2022-09-21 12:54:28 +0300

    Initial

--- src/boards/smnt-mb/platform_io.h
+++ src/boards/smnt-mb/platform_io.h
@@ -37,6 +37,7 @@
 // 1: External I2C ADC on SM1
 // TODO external needs a better fake-pointer-marker than NULL probably
 #define PLATFORM_ADCS { ADC0, NULL }
+#define DMA_ADC ADC0
 
 // 5V regulator (always PWM mode)
 #define POWER_CONTROL_5V_ENABLE_PORT gpioPortB
@@ -51,31 +52,19 @@
 #define POWER_CONTROL_3V3_PWM_MODE_ENABLE_PIN 4
 
 // Microwave sensor enable
-#define SEMW_SENSOR_ENABLE_PORT gpioPortD
-#define SEMW_SENSOR_ENABLE_PIN 12
+#define MICROWAVE_SENSOR_ENABLE_PORT gpioPortD
+#define MICROWAVE_SENSOR_ENABLE_PIN 12
 
 // Microwave sensor ADC
-#define SEMW_SENSOR_INPUT_PORT gpioPortD
-#define SEMW_SENSOR_INPUT_PIN 14
-
-#define SEMW_INPUT_CONFIG_INIT_DEFAULT    \
-{                                         \
-	.gpio_port = SEMW_SENSOR_INPUT_PORT,  \
-	.gpio_pin = SEMW_SENSOR_INPUT_PIN,    \
-	.adc_scan_group = adcScanInputGroup0, \
-	.adc_pos_sel = adcPosSelAPORT3XCH6,   \
-}
+#define MICROWAVE_SENSOR_ADC_INPUT_PORT gpioPortD
+#define MICROWAVE_SENSOR_ADC_INPUT_PIN 14
+#define MICROWAVE_SENSOR_ADC_SCAN_GROUP adcScanInputGroup0
+#define MICROWAVE_SENSOR_ADC_POS_SEL adcPosSelAPORT3XCH6
 
 // Noise sensor ADC
-#define SENO_SENSOR_INPUT_PORT gpioPortD
-#define SENO_SENSOR_INPUT_PIN 13
-
-#define SENO_INPUT_CONFIG_INIT_DEFAULT    \
-{                                         \
-	.gpio_port = SENO_SENSOR_INPUT_PORT,  \
-	.gpio_pin = SENO_SENSOR_INPUT_PIN,    \
-	.adc_scan_group = adcScanInputGroup0, \
-	.adc_pos_sel = adcPosSelAPORT4XCH5,   \
-}
+#define NOISE_SENSOR_ADC_INPUT_PORT gpioPortD
+#define NOISE_SENSOR_ADC_INPUT_PIN 13
+#define NOISE_SENSOR_ADC_SCAN_GROUP adcScanInputGroup0
+#define NOISE_SENSOR_ADC_POS_SEL adcPosSelAPORT4XCH5
 
 #endif//PLATFORM_IO_H_
